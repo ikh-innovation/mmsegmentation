f300879f5fef9838b333a9486a87740776ca6696
thanos <tsakelliou@iknowhow.com>
2022-09-09 15:56:19 +0300

    new config segformer b5

--- configs/heron/grid_search_segformer/segformer_mit-b0_dice_OHEM_640x640_cracks_and_potholes.py
+++ configs/heron/grid_search_segformer/segformer_mit-b0_dice_OHEM_640x640_cracks_and_potholes.py
@@ -1,7 +1,7 @@
 _base_ = ['../segformer_mit-b0_640x640_cracks_and_potholes.py']
 log_config = dict(hooks=[dict(type='MMSegWandbHook', init_kwargs={'entity': "iknowhow",
                                                                   'project': "crack-segmentation",
-                                                                  'name': 'segformer_mit-b0_dice_ignore_640x640_cracks_and_potholes'})])
+                                                                  'name': 'segformer_mit-b0_dice__640x640_cracks_and_potholes'})])
 
 model = dict(
     decode_head=dict(
